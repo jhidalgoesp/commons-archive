fb25b5d3f5465a3c4f7e5c8f6965bbb72f7558ec
Jose Hidalgo <jhidalgoesp@gmail.com>
2022-09-06 11:34:00 -0500

    Adding method to retrieve params from route

--- web/request.go
+++ web/request.go
@@ -2,8 +2,15 @@ package web
 
 import (
 	"encoding/json"
+	"github.com/dimfeld/httptreemux/v5"
 	"net/http"
 )
+
+// Param returns the web call parameters from the request.
+func Param(r *http.Request, key string) string {
+	m := httptreemux.ContextParams(r.Context())
+	return m[key]
+}
 
 // Decode reads the body of an HTTP request looking for a JSON document. The
 // body is decoded into the provided value.
